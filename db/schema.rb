1b8079e51c407c663f7bcaf491e63d6115ba9cec
Victor V <kamusin@gmail.com>
2014-05-16 15:34:00 -0400

    password encryption user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140516155829) do
+ActiveRecord::Schema.define(version: 20140516180909) do
 
   create_table "players", force: true do |t|
     t.string   "name"
@@ -30,6 +30,14 @@ ActiveRecord::Schema.define(version: 20140516155829) do
     t.integer  "player_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "username"
+    t.string   "email"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "password_digest"
   end
 
 end
